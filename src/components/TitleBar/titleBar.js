17441d85fea2dc12774fa9d176411e2cfd7f2a82
GRy82 <gwryan@buffalo.edu>
2021-02-25 16:59:42 -0500

    React application started. index.js designated at the root. TitleBar component created.

---
+++ src/components/TitleBar/titleBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function TitleBar(props) { 
+    return (
+        <div className="row row-spacer">
+            <div className="col-md-12" style={{padding: 0}}>
+                <div>
+                    <h1>Greg Library</h1>
+                </div>
+            </div>
+        </div> 
+    );
+}
+
+export default TitleBar;
